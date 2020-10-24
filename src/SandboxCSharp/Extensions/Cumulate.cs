6f6e1c1f6aaa62931cb11960e40b86396b768718
AconCavy <aconcavy@gmail.com>
2020-10-25 07:27:34 +0900

    Modify Cumulate to check null arguments in eager loading and add tests

--- src/SandboxCSharp/Extensions/Cumulate.cs
+++ src/SandboxCSharp/Extensions/Cumulate.cs
@@ -10,15 +10,30 @@ namespace SandboxCSharp.Extensions
         {
             if (source == null) throw new ArgumentNullException(nameof(source));
             if (func == null) throw new ArgumentNullException(nameof(func));
-            yield return seed;
-            using var enumerator = source.GetEnumerator();
-            while (enumerator.MoveNext()) yield return seed = func(seed, enumerator.Current);
+            return CumulateImpl(source, seed, func);
         }
 
         public static IEnumerable<TAccumulate> Cumulate<TSource, TAccumulate>(this IEnumerable<TSource> source,
-            Func<TAccumulate, TSource, TAccumulate> func) => source.Cumulate(default, func);
+            Func<TAccumulate, TSource, TAccumulate> func)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (func == null) throw new ArgumentNullException(nameof(func));
+            return CumulateImpl(source, default, func);
+        }
 
         public static IEnumerable<TSource> Cumulate<TSource>(this IEnumerable<TSource> source,
-            Func<TSource, TSource, TSource> func) => source.Cumulate(default, func);
+            Func<TSource, TSource, TSource> func)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (func == null) throw new ArgumentNullException(nameof(func));
+            return CumulateImpl(source, default, func);
+        }
+
+        private static IEnumerable<TAccumulate> CumulateImpl<TSource, TAccumulate>(this IEnumerable<TSource> source,
+            TAccumulate seed, Func<TAccumulate, TSource, TAccumulate> func)
+        {
+            yield return seed;
+            foreach (var item in source) yield return seed = func(seed, item);
+        }
     }
 }
