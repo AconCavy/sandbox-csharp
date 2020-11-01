7d756a9364bf636731d9ebd2077f9f189de5c861
AconCavy <aconcavy@gmail.com>
2020-11-02 00:32:59 +0900

    Add MinBy and tests

---
+++ src/SandboxCSharp/Extensions/MinBy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SandboxCSharp.Extensions
+{
+    public static partial class EnumerableExtension
+    {
+        public static TSource MinBy<TSource, TKey>(this IEnumerable<TSource> source,
+            Func<TSource, TKey> selector, bool updateEquals = false) where TKey : IComparable<TKey>
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (selector == null) throw new ArgumentNullException(nameof(selector));
+
+            TSource Inner()
+            {
+                using var e = source.GetEnumerator();
+                if (!e.MoveNext()) throw new InvalidOperationException();
+                var ret = e.Current;
+                var min = selector(ret);
+                while (e.MoveNext())
+                {
+                    var current = e.Current;
+                    var value = selector(current);
+                    if (updateEquals && value.CompareTo(min) > 0) continue;
+                    if (!updateEquals && value.CompareTo(min) >= 0) continue;
+                    ret = current;
+                    min = value;
+                }
+
+                return ret;
+            }
+
+            return Inner();
+        }
+    }
+}
---
+++ tests/SandboxCSharp.Tests/MinByTests.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using SandboxCSharp.Extensions;
+
+namespace SandboxCSharp.Tests
+{
+    public class MinByTests
+    {
+        [Test]
+        public void MinByTestByClass()
+        {
+            const int count = 10;
+            var items = Enumerable.Range(1, count).Select(x => new SampleClass(x, count)).ToArray();
+            var expected = items[0];
+
+            var actual = items.MinBy(x => x.A);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void MinByTestByStruct()
+        {
+            const int count = 10;
+            var items = Enumerable.Range(1, count).Select(x => new SampleStruct(x, count)).ToArray();
+            var expected = items[0];
+
+            var actual = items.MinBy(x => x.A);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void UpdateEqualsFalseTestByClass()
+        {
+            const int count = 10;
+            var items = Enumerable.Range(1, count).Select(x => new SampleClass(x, count)).ToArray();
+            var expected = items[0];
+
+            var actual = items.MinBy(x => x.B);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void UpdateEqualsFalseTestByStruct()
+        {
+            const int count = 10;
+            var items = Enumerable.Range(1, count).Select(x => new SampleStruct(x, count)).ToArray();
+            var expected = items[0];
+
+            var actual = items.MinBy(x => x.B);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void UpdateEqualsTrueTestByClass()
+        {
+            const int count = 10;
+            var items = Enumerable.Range(1, count).Select(x => new SampleClass(x, count)).ToArray();
+            var expected = items[^1];
+
+            var actual = items.MinBy(x => x.B, true);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void UpdateEqualsTrueTestByStruct()
+        {
+            const int count = 10;
+            var items = Enumerable.Range(1, count).Select(x => new SampleStruct(x, count)).ToArray();
+            var expected = items[^1];
+
+            var actual = items.MinBy(x => x.B, true);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void NullSourceTest()
+        {
+            IEnumerable<SampleClass> classes = null;
+            Assert.Throws<ArgumentNullException>(() => classes.MinBy(x => x.A));
+
+            IEnumerable<SampleStruct> structs = null;
+            Assert.Throws<ArgumentNullException>(() => structs.MinBy(x => x.A));
+        }
+
+        [Test]
+        public void NullFuncTest()
+        {
+            var classes = Enumerable.Range(1, 5).Select(x => new SampleClass(x, x));
+            Assert.Throws<ArgumentNullException>(() => classes.MinBy<SampleClass, int>(null));
+
+            var structs = Enumerable.Range(1, 5).Select(x => new SampleStruct(x, x));
+            Assert.Throws<ArgumentNullException>(() => structs.MinBy<SampleStruct, int>(null));
+        }
+
+        [Test]
+        public void NoElementTest()
+        {
+            IEnumerable<SampleClass> classes = new SampleClass[0];
+            Assert.Throws<InvalidOperationException>(() => classes.MinBy(x => x.A));
+
+            IEnumerable<SampleStruct> structs = new SampleStruct[0];
+            Assert.Throws<InvalidOperationException>(() => structs.MinBy(x => x.A));
+        }
+
+        private struct SampleStruct
+        {
+            public int A;
+            public int B;
+            public SampleStruct(int a, int b) => (A, B) = (a, b);
+        }
+
+        private class SampleClass
+        {
+            public int A { get; set; }
+            public int B { get; set; }
+            public SampleClass(int a, int b) => (A, B) = (a, b);
+        }
+    }
+}
