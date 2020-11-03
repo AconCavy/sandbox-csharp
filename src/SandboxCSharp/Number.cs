76ce7558411c4da3d2b235f13ea9bba386941204
AconCavy <aconcavy@gmail.com>
2020-11-04 01:23:42 +0900

    Add Number and tests

---
+++ src/SandboxCSharp/Number.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace SandboxCSharp
+{
+    public static class Number
+    {
+        public static bool IsMultipleOf(ReadOnlySpan<char> value, uint p, bool checkValues = false)
+        {
+            if (value.Length == 0) throw new ArgumentException(nameof(value));
+            if (checkValues)
+            {
+                foreach (var c in value)
+                    if (c < '0' || '9' < c)
+                        throw new ArgumentException(nameof(value));
+            }
+#if DEBUG
+            const int parsableDigit = 3;
+#else
+            const int parsableDigit = 18;
+#endif
+            const int stackSize = 1 << 12;
+            switch (p)
+            {
+                case 0: throw new ArgumentException(nameof(p));
+                case 1: return true;
+                case 2:
+                case 5:
+                case 10: return (value[^1] - '0') % p == 0;
+            }
+
+            if (value.Length <= parsableDigit) return ulong.Parse(value) % p == 0;
+            var v1 = value.Length <= stackSize ? stackalloc sbyte[value.Length] : new sbyte[value.Length];
+            for (var i = 0; i < value.Length; i++) v1[i] = (sbyte) (value[i] - '0');
+            var x = 0L;
+            switch (p)
+            {
+                case 3:
+                case 9:
+                    foreach (var v in v1) x += v;
+                    return x % p == 0;
+                case 6:
+                    foreach (var v in v1) x += v;
+                    return v1[^1] % 2 == 0 && x % 3 == 0;
+                case 4:
+                case 8:
+                    x = v1[^2] * 10 + v1[^1];
+                    if (p == 8) x += v1[^3] * 100;
+                    return x % p == 0;
+            }
+
+            var n = 0;
+            while ((10 * n + 1) % p != 0) n++;
+            var idx = v1.Length - 1;
+            while (idx >= parsableDigit)
+            {
+                var size = 0;
+                var y = v1[idx] * n;
+                while (y > 0)
+                {
+                    v1[idx - size - 1] -= (sbyte) (y % 10);
+                    y /= 10;
+                    size++;
+                }
+
+                for (var i = 0; i <= size; i++)
+                {
+                    if (v1[idx - i] >= 0) continue;
+                    v1[idx - i - 1]--;
+                    v1[idx - i] += 10;
+                }
+
+                idx--;
+            }
+
+            x = 0;
+            foreach (var v in v1[..(idx + 1)])
+            {
+                x *= 10;
+                x += v;
+            }
+
+            return x % p == 0;
+        }
+
+        public static int[] ToDigits(long value)
+        {
+            if (value == 0) return new[] {0};
+            value = Math.Abs(value);
+            const int size = 32;
+            Span<int> ret = stackalloc int[size];
+            var idx = 1;
+            while (value > 0)
+            {
+                ret[^idx] = (int) (value % 10);
+                value /= 10;
+                idx++;
+            }
+
+            return ret.Slice(size - idx + 1).ToArray();
+        }
+    }
+}
---
+++ tests/SandboxCSharp.Tests/NumberTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace SandboxCSharp.Tests
+{
+    public class NumberTests
+    {
+        [Test]
+        public void IsMultipleOfTest([Range(1, 11)] int p)
+        {
+            for (var i = 0; i < 1 << 16; i++)
+            {
+                var str = i.ToString();
+                var expected = i % p == 0;
+                var actual = Number.IsMultipleOf(str, (uint) p);
+                Assert.That(actual, Is.EqualTo(expected));
+            }
+        }
+
+        [Test]
+        public void IsMultipleOfZeroTest()
+        {
+            const string str = "1234567890";
+            Assert.Throws<ArgumentException>(() => Number.IsMultipleOf(str, 0));
+        }
+
+        [Test]
+        public void IsMultipleOfStringIncludesOutOfNumberTest()
+        {
+            const string str = "1234a67890";
+            Assert.Throws<ArgumentException>(() => Number.IsMultipleOf(str, 2, true));
+        }
+
+        [Test]
+        public void IsMultipleOfEmptyStringTest()
+        {
+            const string str = "";
+            Assert.Throws<ArgumentException>(() => Number.IsMultipleOf(str, 2, true));
+        }
+
+        [Test]
+        public void IsMultipleOfNullStringTest()
+        {
+            const string str = null;
+            Assert.Throws<ArgumentException>(() => Number.IsMultipleOf(str, 2, true));
+        }
+
+        [Test]
+        public void ToDigitsTest1()
+        {
+            for (var i = 0; i < 1 << 16; i++)
+            {
+                var expected = i.ToString().Select(x => x - '0');
+                var actual = Number.ToDigits(i);
+                Assert.That(actual, Is.EqualTo(expected));
+            }
+        }
+
+        [Test]
+        public void ToDigitsTest2()
+        {
+            for (var i = 0; i < 63; i++)
+            {
+                var n = 1L << i;
+                var expected = n.ToString().Select(x => x - '0');
+                var actual = Number.ToDigits(n);
+                Assert.That(actual, Is.EqualTo(expected));
+            }
+        }
+    }
+}
