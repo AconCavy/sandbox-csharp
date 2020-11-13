11e59ac56091edae6ee3b5841ca14c319754b077
AconCavy <aconcavy@gmail.com>
2020-11-14 03:29:42 +0900

    Add TryGetLast and tests

---
+++ src/SandboxCSharp/Extensions/TryGetLast.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SandboxCSharp.Extensions
+{
+    public static partial class EnumerableExtension
+    {
+        public static bool TryGetLast<TSource>(this IEnumerable<TSource> source, out TSource result)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            using var e = source.GetEnumerator();
+            var exist = false;
+            result = default;
+            while (e.MoveNext())
+            {
+                result = e.Current;
+                exist = true;
+            }
+
+            return exist;
+        }
+
+        public static bool TryGetLast<TSource>(this IEnumerable<TSource> source, out TSource result,
+            Predicate<TSource> predicate)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (predicate == null) throw new ArgumentNullException(nameof(predicate));
+            using var e = source.GetEnumerator();
+            var exist = false;
+            result = default;
+            while (e.MoveNext())
+            {
+                var current = e.Current;
+                if (!predicate(current)) continue;
+                result = current;
+                exist = true;
+            }
+
+            return exist;
+        }
+    }
+}
