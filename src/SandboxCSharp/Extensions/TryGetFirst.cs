2ed696721e4ff1d2efef32a6962f42fb80444f3e
AconCavy <aconcavy@gmail.com>
2020-11-14 03:29:26 +0900

    Add TryGetFirst and tests

---
+++ src/SandboxCSharp/Extensions/TryGetFirst.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SandboxCSharp.Extensions
+{
+    public static partial class EnumerableExtension
+    {
+        public static bool TryGetFirst<TSource>(this IEnumerable<TSource> source, out TSource result)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            using var e = source.GetEnumerator();
+            var exist = e.MoveNext();
+            result = exist ? e.Current : default;
+            return exist;
+        }
+
+        public static bool TryGetFirst<TSource>(this IEnumerable<TSource> source, out TSource result,
+            Predicate<TSource> predicate)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (predicate == null) throw new ArgumentNullException(nameof(predicate));
+            using var e = source.GetEnumerator();
+            while (e.MoveNext())
+            {
+                var current = e.Current;
+                if (!predicate(current)) continue;
+                result = e.Current;
+                return true;
+            }
+
+            result = default;
+            return false;
+        }
+    }
+}
