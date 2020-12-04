579636aded9e1b0bc1a9ab583877529bbd2933d0
AconCavy <aconcavy@gmail.com>
2020-12-04 21:44:12 +0900

    Update NQueensProblem

--- src/SandboxCSharp/NQueensProblem.cs
+++ src/SandboxCSharp/NQueensProblem.cs
@@ -7,83 +7,80 @@ namespace SandboxCSharp
 {
     public static class NQueensProblem
     {
-        public static IEnumerable<bool[][]> Permute(int count)
+        public static IEnumerable<bool[][]> Permute(int size)
         {
-            foreach (var permutation in Enumerable.Range(0, count).Permute(count))
+            foreach (var permutation in Enumerable.Range(0, size).Permute(size))
             {
-                var answer = new bool[count][].Select(_ => new bool[count]).ToArray();
-                var isDuplicated = false;
-                foreach (var (x, i) in permutation.Select((x, i) => (x, i)))
+                var answer = new bool[size][].Select(_ => new bool[size]).ToArray();
+                var isSatisfiable = true;
+                foreach (var (r, c) in permutation.Select((x, i) => (i, x)))
                 {
-                    answer[i][x] = true;
-                    for (var d = 1; d < count && !isDuplicated; d++)
-                    {
-                        isDuplicated |= i + d < count && x + d < count && answer[i + d][x + d];
-                        isDuplicated |= i - d >= 0 && x + d < count && answer[i - d][x + d];
-                        isDuplicated |= i + d < count && x - d >= 0 && answer[i + d][x - d];
-                        isDuplicated |= i - d >= 0 && x - d >= 0 && answer[i - d][x - d];
-                    }
+                    answer[r][c] = true;
+                    isSatisfiable &= CanPlace(answer, r, c);
+                    if (!isSatisfiable) break;
                 }
 
-                if (!isDuplicated) yield return answer;
+                if (isSatisfiable) yield return answer;
             }
         }
 
-        public static (bool isIdentified, bool[][] answer) Identify(int count,
+        public static (bool isIdentified, bool[][] answer) Identify(int size,
             IEnumerable<(int row, int column)> constraints = null)
         {
-            var answer = new bool[count][].Select(_ => new bool[count]).ToArray();
-            constraints ??= Array.Empty<(int, int)>();
-            var ok = 0;
-            var isIdentified = false;
-            foreach (var (r, c) in constraints)
-            {
-                answer[r][c] = true;
-                ok++;
-            }
+            var answer = new bool[size][].Select(_ => new bool[size]).ToArray();
+            var queens = constraints?.ToArray() ?? Array.Empty<(int, int)>();
+            foreach (var (r, c) in queens) answer[r][c] = true;
+            if (queens.Any(x => !CanPlace(answer, x.row, x.column))) return (false, answer);
 
-            void Inner()
+            var isIdentified = false;
+
+            void Inner(int placed)
             {
                 if (isIdentified) return;
-                if (ok == count)
+                if (placed == size)
                 {
                     isIdentified = true;
                     return;
                 }
 
-                for (var r = 0; r < count; r++)
+                for (var r = 0; r < size; r++)
                 {
-                    for (var c = 0; c < count; c++)
+                    for (var c = 0; c < size; c++)
                     {
-                        if (answer[r][c]) continue;
-                        var isDuplicated = false;
-                        for (var i = 0; i < count && !isDuplicated; i++)
-                        {
-                            isDuplicated |= answer[i][c];
-                            isDuplicated |= answer[r][i];
-                        }
-
-                        for (var d = 1; d < count && !isDuplicated; d++)
-                        {
-                            isDuplicated |= r + d < count && c + d < count && answer[r + d][c + d];
-                            isDuplicated |= r - d >= 0 && c - d >= 0 && answer[r - d][c - d];
-                            isDuplicated |= r + d < count && c - d >= 0 && answer[r + d][c - d];
-                            isDuplicated |= r - d >= 0 && c + d < count && answer[r - d][c + d];
-                        }
-
-                        if (isDuplicated) continue;
+                        if (answer[r][c] || !CanPlace(answer, r, c)) continue;
                         answer[r][c] = true;
-                        ok++;
-                        Inner();
+                        Inner(placed + 1);
                         if (isIdentified) return;
                         answer[r][c] = false;
-                        ok--;
                     }
                 }
             }
 
-            Inner();
+            Inner(queens.Length);
             return (isIdentified, answer);
+        }
+
+        public static bool CanPlace(in bool[][] grid, int r, int c)
+        {
+            var size = grid.Length;
+            var d4 = new[] {(1, 1), (1, -1), (-1, 1), (-1, -1)};
+            for (var i = 1; i < size; i++)
+            {
+                foreach (var (dr, dc) in d4)
+                {
+                    var (nr, nc) = (r + dr * i, c + dc * i);
+                    if (nr < 0 || size <= nr || nc < 0 || size <= nc) continue;
+                    if (grid[nr][nc]) return false;
+                }
+            }
+
+            for (var i = 0; i < size; i++)
+            {
+                if (i != c && grid[r][i]) return false;
+                if (i != r && grid[i][c]) return false;
+            }
+
+            return true;
         }
     }
 }
