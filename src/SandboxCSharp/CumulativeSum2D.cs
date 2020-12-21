9518b48db6a3b82025abde9b2f6a7affaaa6f320
AconCavy <aconcavy@gmail.com>
2020-12-22 02:45:23 +0900

    Update CumulativeSum2D

--- src/SandboxCSharp/CumulativeSum2D.cs
+++ src/SandboxCSharp/CumulativeSum2D.cs
@@ -2,42 +2,48 @@
 {
     public class CumulativeSum2D
     {
-        private readonly long[,] _graph;
+        private readonly int _height;
+        private readonly int _width;
+        private readonly long[,] _data;
         private readonly long[,] _sum;
         private bool _isUpdated;
 
-        public CumulativeSum2D(int x, int y)
+        public CumulativeSum2D(int h, int w)
         {
-            _graph = new long[y, x];
-            _sum = new long[y + 1, x + 1];
+            _height = h;
+            _width = w;
+            _data = new long[h, w];
+            _sum = new long[h + 1, w + 1];
         }
 
-        public long this[int y, int x]
+        public void Add(int h, int w, long value)
         {
-            get => _graph[y, x];
-            set
-            {
-                _isUpdated = false;
-                _graph[y, x] = value;
-            }
+            _isUpdated = false;
+            _data[h, w] += value;
         }
 
-        public void Add(int x, int y, long value)
+        public void Set(int h, int w, long value)
         {
-            _graph[y, x] += value;
+            _isUpdated = false;
+            _data[h, w] = value;
         }
 
-        public long GetArea(int x1, int y1, int x2, int y2)
+        public long Get(int h, int w)
+        {
+            return _data[h, w];
+        }
+
+        public long Sum(int h1, int w1, int h2, int w2)
         {
             if (!_isUpdated) Build();
-            return _sum[y1, x1] + _sum[y2, x2] - _sum[y2, x1] - _sum[y1, x2];
+            return _sum[h1, w1] + _sum[h2, w2] - _sum[h2, w1] - _sum[h1, w2];
         }
 
         private void Build()
         {
-            for (var i = 1; i < _sum.GetLength(0); i++)
-            for (var j = 1; j < _sum.GetLength(1); j++)
-                _sum[i, j] = _sum[i, j - 1] + _sum[i - 1, j] - _sum[i - 1, j - 1] + _graph[i - 1, j - 1];
+            for (var i = 1; i < _height; i++)
+            for (var j = 1; j < _width; j++)
+                _sum[i, j] = _sum[i, j - 1] + _sum[i - 1, j] - _sum[i - 1, j - 1] + _data[i - 1, j - 1];
             _isUpdated = true;
         }
     }
