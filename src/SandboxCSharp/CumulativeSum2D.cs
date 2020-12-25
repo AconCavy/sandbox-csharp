d8ce1cf0f4937ff20fc849cef4a7fe153a769364
AconCavy <aconcavy@gmail.com>
2020-12-26 01:04:58 +0900

    Update CumulativeSum2D and add tests

--- src/SandboxCSharp/CumulativeSum2D.cs
+++ src/SandboxCSharp/CumulativeSum2D.cs
@@ -1,4 +1,6 @@
-﻿namespace SandboxCSharp
+﻿using System;
+
+namespace SandboxCSharp
 {
     public class CumulativeSum2D
     {
@@ -10,6 +12,8 @@
 
         public CumulativeSum2D(int h, int w)
         {
+            if (h <= 0) throw new ArgumentOutOfRangeException(nameof(h));
+            if (w <= 0) throw new ArgumentOutOfRangeException(nameof(w));
             _height = h;
             _width = w;
             _data = new long[h, w];
@@ -18,33 +22,53 @@
 
         public void Add(int h, int w, long value)
         {
+            if (h < 0 || _height <= h) throw new ArgumentOutOfRangeException(nameof(h));
+            if (w < 0 || _width <= w) throw new ArgumentOutOfRangeException(nameof(w));
             _isUpdated = false;
             _data[h, w] += value;
         }
 
         public void Set(int h, int w, long value)
         {
+            if (h < 0 || _height <= h) throw new ArgumentOutOfRangeException(nameof(h));
+            if (w < 0 || _width <= w) throw new ArgumentOutOfRangeException(nameof(w));
             _isUpdated = false;
             _data[h, w] = value;
         }
 
         public long Get(int h, int w)
         {
+            if (h < 0 || _height <= h) throw new ArgumentOutOfRangeException(nameof(h));
+            if (w < 0 || _width <= w) throw new ArgumentOutOfRangeException(nameof(w));
             return _data[h, w];
         }
 
+        public long Sum(int h, int w)
+        {
+            if (h < 0 || _height <= h) throw new ArgumentOutOfRangeException(nameof(h));
+            if (w < 0 || _width <= w) throw new ArgumentOutOfRangeException(nameof(w));
+            if (!_isUpdated) Build();
+            return _sum[h + 1, w + 1];
+        }
+
+
         public long Sum(int h1, int w1, int h2, int w2)
         {
+            if (h1 < 0 || _height <= h1) throw new ArgumentOutOfRangeException(nameof(h1));
+            if (w1 < 0 || _width <= w1) throw new ArgumentOutOfRangeException(nameof(w1));
+            if (h2 < 0 || _height <= h2) throw new ArgumentOutOfRangeException(nameof(h2));
+            if (w2 < 0 || _width <= w2) throw new ArgumentOutOfRangeException(nameof(w2));
             if (!_isUpdated) Build();
-            return _sum[h1, w1] + _sum[h2, w2] - _sum[h2, w1] - _sum[h1, w2];
+            return _sum[h1 + 1, w1 + 1] + _sum[h2 + 1, w2 + 1] - _sum[h2 + 1, w1 + 1] - _sum[h1 + 1, w2 + 1];
         }
 
         private void Build()
         {
-            for (var i = 1; i < _height; i++)
-            for (var j = 1; j < _width; j++)
-                _sum[i, j] = _sum[i, j - 1] + _sum[i - 1, j] - _sum[i - 1, j - 1] + _data[i - 1, j - 1];
             _isUpdated = true;
+            _sum[0, 0] = _sum[1, 0] = _sum[0, 1] = 0;
+            for (var i = 1; i <= _height; i++)
+            for (var j = 1; j <= _width; j++)
+                _sum[i, j] = _sum[i, j - 1] + _sum[i - 1, j] - _sum[i - 1, j - 1] + _data[i - 1, j - 1];
         }
     }
 }
---
+++ tests/SandboxCSharp.Tests/CumulativeSum2DTests.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SandboxCSharp.Tests
+{
+    public class CumulativeSum2DTests
+    {
+        [Test]
+        public void InitializeTest()
+        {
+            Assert.DoesNotThrow(() => _ = new CumulativeSum2D(1, 1));
+            Assert.Throws<ArgumentOutOfRangeException>(() => _ = new CumulativeSum2D(0, 1));
+            Assert.Throws<ArgumentOutOfRangeException>(() => _ = new CumulativeSum2D(1, 0));
+        }
+
+        [TestCase(-1, 0)]
+        [TestCase(0, -1)]
+        public void ArgumentOutOfRangeTest(int h, int w)
+        {
+            var cum = new CumulativeSum2D(1, 1);
+            Assert.Throws<ArgumentOutOfRangeException>(() => cum.Add(h, w, 1));
+            Assert.Throws<ArgumentOutOfRangeException>(() => cum.Set(h, w, 1));
+            Assert.Throws<ArgumentOutOfRangeException>(() => cum.Get(h, w));
+            Assert.Throws<ArgumentOutOfRangeException>(() => cum.Sum(h, w));
+            Assert.Throws<ArgumentOutOfRangeException>(() => cum.Sum(h, w, 0, 0));
+            Assert.Throws<ArgumentOutOfRangeException>(() => cum.Sum(0, 0, h, w));
+        }
+
+        [Test]
+        public void SetAndAddAndGetTest()
+        {
+            var cum = new CumulativeSum2D(1, 1);
+            var expected = 1;
+            cum.Set(0, 0, expected);
+            var actual = cum.Get(0, 0);
+            Assert.That(actual, Is.EqualTo(expected));
+
+            cum.Add(0, 0, expected);
+            expected += expected;
+            actual = cum.Get(0, 0);
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void SumTest()
+        {
+            const int height = 3;
+            const int width = 3;
+            var cum = new CumulativeSum2D(height, width);
+            for (var i = 0; i < height; i++)
+            for (var j = 0; j < width; j++)
+                cum.Set(i, j, 1);
+
+            for (var i = 0; i < height; i++)
+            for (var j = 0; j < width; j++)
+            {
+                var expected = (i + 1) * (j + 1);
+                var actual = cum.Sum(i, j);
+                Assert.That(actual, Is.EqualTo(expected));
+            }
+        }
+
+        [Test]
+        public void MatchTest()
+        {
+            var random = new Random(0);
+            const int height = 50;
+            const int width = 50;
+            var ft = new FenwickTree2D(height, width);
+            var cum = new CumulativeSum2D(height, width);
+
+            for (var i = 0; i < height; i++)
+            for (var j = 0; j < width; j++)
+            {
+                var value = random.Next(0, 10);
+                ft.Add(i, j, value);
+                cum.Add(i, j, value);
+            }
+
+            for (var h1 = 0; h1 < height; h1++)
+            for (var w1 = 0; w1 < width; w1++)
+            for (var h2 = 0; h2 <= h1; h2++)
+            for (var w2 = 0; w2 <= w1; w2++)
+            {
+                var sum1 = ft.Sum(h1, w1, h2, w2);
+                var sum2 = cum.Sum(h1, w1, h2, w2);
+                Assert.That(sum1, Is.EqualTo(sum2));
+            }
+        }
+    }
+}
