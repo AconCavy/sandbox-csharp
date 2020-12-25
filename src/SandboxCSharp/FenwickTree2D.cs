d3c89fe863b59f6277f56253a45747c5d155182a
AconCavy <aconcavy@gmail.com>
2020-12-26 01:05:12 +0900

    Update FenwickTree2D and add tests

--- src/SandboxCSharp/FenwickTree2D.cs
+++ src/SandboxCSharp/FenwickTree2D.cs
@@ -1,4 +1,6 @@
-﻿namespace SandboxCSharp
+﻿using System;
+
+namespace SandboxCSharp
 {
     public class FenwickTree2D
     {
@@ -8,6 +10,8 @@
 
         public FenwickTree2D(int h, int w)
         {
+            if (h <= 0) throw new ArgumentOutOfRangeException(nameof(h));
+            if (w <= 0) throw new ArgumentOutOfRangeException(nameof(w));
             _height = h;
             _width = w;
             _data = new long[_height, _width];
@@ -15,6 +19,8 @@
 
         public void Add(int h, int w, long value)
         {
+            if (h < 0 || _height <= h) throw new ArgumentOutOfRangeException(nameof(h));
+            if (w < 0 || _width <= w) throw new ArgumentOutOfRangeException(nameof(w));
             for (var i = h + 1; i <= _height; i += i & -i)
             for (var j = w + 1; j <= _width; j += j & -j)
                 _data[i - 1, j - 1] += value;
@@ -22,6 +28,8 @@
 
         public long Sum(int h, int w)
         {
+            if (h < 0 || _height <= h) throw new ArgumentOutOfRangeException(nameof(h));
+            if (w < 0 || _width <= w) throw new ArgumentOutOfRangeException(nameof(w));
             var sum = 0L;
             for (var i = h + 1; i > 0; i -= i & -i)
             for (var j = w + 1; j > 0; j -= j & -j)
@@ -31,7 +39,11 @@
 
         public long Sum(int h1, int w1, int h2, int w2)
         {
-            return Sum(h2, w2) - Sum(h2, w1) - Sum(h1, w2) + Sum(h1, w1);
+            if (h1 < 0 || _height <= h1) throw new ArgumentOutOfRangeException(nameof(h1));
+            if (w1 < 0 || _width <= w1) throw new ArgumentOutOfRangeException(nameof(w1));
+            if (h2 < 0 || _height <= h2) throw new ArgumentOutOfRangeException(nameof(h2));
+            if (w2 < 0 || _width <= w2) throw new ArgumentOutOfRangeException(nameof(w2));
+            return Sum(h1, w1) + Sum(h2, w2) - Sum(h2, w1) - Sum(h1, w2);
         }
     }
 }
---
+++ tests/SandboxCSharp.Tests/FenwickTree2DTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SandboxCSharp.Tests
+{
+    public class FenwickTree2DTests
+    {
+        [Test]
+        public void InitializeTest()
+        {
+            Assert.DoesNotThrow(() => _ = new FenwickTree2D(1, 1));
+            Assert.Throws<ArgumentOutOfRangeException>(() => _ = new FenwickTree2D(0, 1));
+            Assert.Throws<ArgumentOutOfRangeException>(() => _ = new FenwickTree2D(1, 0));
+        }
+
+        [TestCase(-1, 0)]
+        [TestCase(0, -1)]
+        public void ArgumentOutOfRangeTest(int h, int w)
+        {
+            var cum = new FenwickTree2D(1, 1);
+            Assert.Throws<ArgumentOutOfRangeException>(() => cum.Add(h, w, 1));
+            Assert.Throws<ArgumentOutOfRangeException>(() => cum.Sum(h, w));
+            Assert.Throws<ArgumentOutOfRangeException>(() => cum.Sum(h, w, 0, 0));
+            Assert.Throws<ArgumentOutOfRangeException>(() => cum.Sum(0, 0, h, w));
+        }
+
+        [Test]
+        public void AddTest()
+        {
+            var cum = new FenwickTree2D(1, 1);
+            const int expected = 1;
+            cum.Add(0, 0, expected);
+            var actual = cum.Sum(0, 0);
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void SumTest()
+        {
+            const int height = 3;
+            const int width = 3;
+            var cum = new FenwickTree2D(height, width);
+            for (var i = 0; i < height; i++)
+            for (var j = 0; j < width; j++)
+                cum.Add(i, j, 1);
+
+            for (var i = 0; i < height; i++)
+            for (var j = 0; j < width; j++)
+            {
+                var expected = (i + 1) * (j + 1);
+                var actual = cum.Sum(i, j);
+                Assert.That(actual, Is.EqualTo(expected));
+            }
+        }
+    }
+}
