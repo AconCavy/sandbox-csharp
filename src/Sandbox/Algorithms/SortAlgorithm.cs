5cbd96808bc4bd2ee8be6a7c47098cb6743ddd90
AconCavy <aconcavy@gmail.com>
2021-08-29 10:24:27 +0900

    Add SortAlgorithm

---
+++ src/Sandbox/Algorithms/SortAlgorithm.cs
@@ -0,0 +1,128 @@
+using System;
+using System.Collections.Generic;
+
+namespace Sandbox.Algorithms
+{
+    public static class SortAlgorithm
+    {
+        public static void Quick<T>(T[] source, int l, int r, Comparison<T> comparison = null)
+        {
+            if (l < 0 || source.Length < r) throw new ArgumentOutOfRangeException();
+            if (r <= l) throw new ArgumentException();
+            comparison ??= Comparer<T>.Default.Compare;
+
+            var pivot = source[(l + r) / 2];
+
+            var (ll, rr) = (l, r - 1);
+            while (true)
+            {
+                while (comparison(pivot, source[ll]) > 0) ll++;
+                while (comparison(pivot, source[rr]) < 0) rr--;
+                if (ll >= rr) break;
+                (source[ll], source[rr]) = (source[rr], source[ll]);
+            }
+
+            if (ll - l > 1) Quick(source, l, ll, comparison);
+            if (r - rr > 1) Quick(source, rr, r, comparison);
+        }
+
+        public static void Bubble<T>(T[] source, int l, int r, Comparison<T> comparison = null)
+        {
+            if (l < 0 || source.Length < r) throw new ArgumentOutOfRangeException();
+            if (r <= l) throw new ArgumentException();
+            comparison ??= Comparer<T>.Default.Compare;
+
+            for (var i = 0; i < r - l; i++)
+            {
+                var ok = true;
+                for (var j = l; j + 1 < r - i; j++)
+                {
+                    if (comparison(source[j], source[j + 1]) <= 0) continue;
+
+                    (source[j], source[j + 1]) = (source[j + 1], source[j]);
+                    ok = false;
+                }
+
+                if (ok) return;
+            }
+        }
+
+        public static void Merge<T>(T[] source, int l, int r, Comparison<T> comparison = null)
+        {
+            if (l < 0 || source.Length < r) throw new ArgumentOutOfRangeException();
+            if (r <= l) throw new ArgumentException();
+            comparison ??= Comparer<T>.Default.Compare;
+
+            var n = r - l;
+            if (n < 2) return;
+
+            var m = (l + r) / 2;
+            Merge(source, l, m, comparison);
+            Merge(source, m, r, comparison);
+
+            var buffer = new T[n];
+            var (ll, rr) = (l, m);
+            var idx = 0;
+            while (ll < m && rr < r)
+            {
+                if (comparison(source[ll], source[rr]) <= 0) buffer[idx++] = source[ll++];
+                else buffer[idx++] = source[rr++];
+            }
+
+            while (ll < m) buffer[idx++] = source[ll++];
+            while (rr < r) buffer[idx++] = source[rr++];
+
+            for (var i = 0; i < n; i++)
+            {
+                source[l + i] = buffer[i];
+            }
+        }
+
+        public static void Insertion<T>(T[] source, int l, int r, Comparison<T> comparison = null)
+        {
+            if (l < 0 || source.Length < r) throw new ArgumentOutOfRangeException();
+            if (r <= l) throw new ArgumentException();
+            comparison ??= Comparer<T>.Default.Compare;
+
+            for (var i = l + 1; i < r; i++)
+            {
+                for (var j = i; j - 1 >= l; j--)
+                {
+                    if (comparison(source[j - 1], source[j]) > 0)
+                        (source[j - 1], source[j]) = (source[j], source[j - 1]);
+                    else break;
+                }
+            }
+        }
+
+        public static void Shell<T>(T[] source, int l, int r, Comparison<T> comparison = null)
+        {
+            if (l < 0 || source.Length < r) throw new ArgumentOutOfRangeException();
+            if (r <= l) throw new ArgumentException();
+            comparison ??= Comparer<T>.Default.Compare;
+
+            var max = -(-(r - l)) / 3;
+            var k = 1;
+            while (k * 3 - 1 <= max * 2) k *= 3;
+            var h = (k - 1) / 2;
+
+            while (h > 0)
+            {
+                for (var d = 0; d < h; d++)
+                {
+                    for (var i = l + d + h; i < r; i++)
+                    {
+                        for (var j = i; j - h >= l; j -= h)
+                        {
+                            if (comparison(source[j - h], source[j]) > 0)
+                                (source[j - h], source[j]) = (source[j], source[j - h]);
+                            else break;
+                        }
+                    }
+                }
+
+                h /= 3;
+            }
+        }
+    }
+}
