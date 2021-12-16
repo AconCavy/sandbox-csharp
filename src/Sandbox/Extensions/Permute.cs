2e600bf3a4429cd01809931e99e32ff6d4485a66
AconCavy <aconcavy@gmail.com>
2021-12-16 18:55:12 +0900

    Update Permute

--- src/Sandbox/Extensions/Permute.cs
+++ src/Sandbox/Extensions/Permute.cs
@@ -6,49 +6,121 @@ namespace Sandbox.Extensions
 {
     public static partial class EnumerableExtension
     {
-        public static IEnumerable<IEnumerable<T>> Permute<T>(this IEnumerable<T> source)
+        public static IEnumerable<T[]> Permute<T>(this IEnumerable<T> source)
         {
-            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (source is null) throw new ArgumentNullException(nameof(source));
 
-            IEnumerable<IEnumerable<T>> Inner()
+            IEnumerable<T[]> Inner()
             {
                 var items = source.ToArray();
-                var ret = new T[items.Length];
-                foreach (var indices in PermuteIndices(items.Length))
+                var n = items.Length;
+                var indices = new int[n];
+                for (var i = 0; i < indices.Length; i++)
                 {
-                    var idx = 0;
-                    foreach (var index in indices) ret[idx++] = items[index];
-                    yield return ret;
+                    indices[i] = i;
+                }
+
+                var result = new T[n];
+
+                void Fill()
+                {
+                    for (var i = 0; i < n; i++)
+                    {
+                        result[i] = items[indices[i]];
+                    }
+                }
+
+                Fill();
+                yield return result;
+                while (true)
+                {
+                    var (i, j) = (n - 2, n - 1);
+                    while (i >= 0)
+                    {
+                        if (indices[i] < indices[i + 1]) break;
+                        i--;
+                    }
+
+                    if (i == -1) yield break;
+                    while (true)
+                    {
+                        if (indices[j] > indices[i]) break;
+                        j--;
+                    }
+
+                    (indices[i], indices[j]) = (indices[j], indices[i]);
+                    Array.Reverse(indices, i + 1, n - 1 - i);
+                    Fill();
+                    yield return result;
                 }
             }
 
             return Inner();
         }
 
-        private static IEnumerable<IEnumerable<int>> PermuteIndices(int n)
+        public static IEnumerable<IEnumerable<T>> Permute<T>(this IEnumerable<T> source, int count)
         {
-            var indices = Enumerable.Range(0, n).ToArray();
-            yield return indices;
-            while (true)
+            if (source is null) throw new ArgumentNullException(nameof(source));
+
+            IEnumerable<T[]> Inner()
             {
-                var (i, j) = (indices.Length - 2, indices.Length - 1);
-                while (i >= 0)
+                var items = source.ToArray();
+                if (count <= 0 || items.Length < count) throw new ArgumentOutOfRangeException(nameof(count));
+                var n = items.Length;
+                var indices = new int[n];
+                for (var i = 0; i < indices.Length; i++)
                 {
-                    if (indices[i] < indices[i + 1]) break;
-                    i--;
+                    indices[i] = i;
                 }
 
-                if (i == -1) yield break;
+                var cycles = new int[count];
+                for (var i = 0; i < cycles.Length; i++)
+                {
+                    cycles[i] = n - i;
+                }
+
+                var result = new T[count];
+
+                void Fill()
+                {
+                    for (var i = 0; i < count; i++)
+                    {
+                        result[i] = items[indices[i]];
+                    }
+                }
+
+                Fill();
+                yield return result;
                 while (true)
                 {
-                    if (indices[j] > indices[i]) break;
-                    j--;
-                }
+                    var done = true;
+                    for (var i = count - 1; i >= 0; i--)
+                    {
+                        cycles[i]--;
+                        if (cycles[i] == 0)
+                        {
+                            for (var j = i; j + 1 < indices.Length; j++)
+                            {
+                                (indices[j], indices[j + 1]) = (indices[j + 1], indices[j]);
+                            }
 
-                (indices[i], indices[j]) = (indices[j], indices[i]);
-                Array.Reverse(indices, i + 1, indices.Length - 1 - i);
-                yield return indices;
+                            cycles[i] = n - i;
+                        }
+                        else
+                        {
+                            (indices[i], indices[^cycles[i]]) = (indices[^cycles[i]], indices[i]);
+                            Fill();
+                            yield return result;
+                            done = false;
+                            break;
+                        }
+                    }
+
+                    if (done) yield break;
+                }
             }
+
+            return Inner();
         }
     }
 }
