af2cd849a043ebf4ebe1b1cfdd0e174b543fb63c
AconCavy <aconcavy@gmail.com>
2022-03-11 15:50:58 +0900

    Remove benchmarks

--- benchmark/Sandbox.Benchmark/Program.cs
+++ benchmark/Sandbox.Benchmark/Program.cs
@@ -5,7 +5,5 @@ namespace Sandbox.Benchmark;
 public static class Program
 {
     private static void Main() =>
-        // BenchmarkRunner.Run<PrimeBenchmark>();
-        // BenchmarkRunner.Run<PermuteBenchmark>();
-        BenchmarkRunner.Run<CombineBenchmark>();
+        BenchmarkRunner.Run<PrimeBenchmark>();
 }
