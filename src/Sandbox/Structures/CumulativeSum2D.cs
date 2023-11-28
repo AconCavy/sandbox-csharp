40cf396aef0d4ec244d98d4d2f8eda1959f87608
AconCavy <aconcavy@gmail.com>
2023-11-28 09:57:29 +0900

    Updated to handle values with INumber interface

--- src/Sandbox/Structures/CumulativeSum2D.cs
+++ src/Sandbox/Structures/CumulativeSum2D.cs
@@ -1,12 +1,14 @@
+using System.Numerics;
+
 namespace Sandbox.Structures;
 
-public class CumulativeSum2D
+public class CumulativeSum2D<T> where T : INumber<T>
 {
     public int Height { get; }
     public int Width { get; }
 
-    private readonly long[] _data;
-    private readonly long[] _sum;
+    private readonly T[] _data;
+    private readonly T[] _sum;
     private bool _isUpdated;
 
     public CumulativeSum2D(int height, int width)
@@ -15,48 +17,59 @@ public class CumulativeSum2D
         if (width <= 0) throw new ArgumentOutOfRangeException(nameof(width));
         Height = height;
         Width = width;
-        _data = new long[height * width];
-        _sum = new long[(height + 1) * (width + 1)];
+        _data = new T[height * width];
+        _sum = new T[(height + 1) * (width + 1)];
     }
 
-    public void Add(int height, int width, long value)
+    public void Add(int height, int width, T value)
     {
-        if (height < 0 || Height <= height) throw new ArgumentOutOfRangeException(nameof(height));
-        if (width < 0 || Width <= width) throw new ArgumentOutOfRangeException(nameof(width));
+        ThrowIfNegative(height);
+        ThrowIfGreaterThanOrEqual(height, Height);
+        ThrowIfNegative(width);
+        ThrowIfGreaterThanOrEqual(width, Width);
         _isUpdated = false;
         _data[height * Width + width] += value;
     }
 
-    public void Set(int height, int width, long value)
+    public void Set(int height, int width, T value)
     {
-        if (height < 0 || Height <= height) throw new ArgumentOutOfRangeException(nameof(height));
-        if (width < 0 || Width <= width) throw new ArgumentOutOfRangeException(nameof(width));
+        ThrowIfNegative(height);
+        ThrowIfGreaterThanOrEqual(height, Height);
+        ThrowIfNegative(width);
+        ThrowIfGreaterThanOrEqual(width, Width);
         _isUpdated = false;
         _data[height * Width + width] = value;
     }
 
-    public long Get(int height, int width)
+    public T Get(int height, int width)
     {
-        if (height < 0 || Height <= height) throw new ArgumentOutOfRangeException(nameof(height));
-        if (width < 0 || Width <= width) throw new ArgumentOutOfRangeException(nameof(width));
+        ThrowIfNegative(height);
+        ThrowIfGreaterThanOrEqual(height, Height);
+        ThrowIfNegative(width);
+        ThrowIfGreaterThanOrEqual(width, Width);
         return _data[height * Width + width];
     }
 
-    public long Sum(int height, int width)
+    /// <summary>
+    /// Calculate a two-dimensional cumulative sum of [0, height), [0, width).
+    /// </summary>
+    public T Sum(int height, int width)
     {
-        if (height < 0 || Height < height) throw new ArgumentOutOfRangeException(nameof(height));
-        if (width < 0 || Width < width) throw new ArgumentOutOfRangeException(nameof(width));
+        ThrowIfNegative(height);
+        ThrowIfGreaterThan(height, Height);
+        ThrowIfNegative(width);
+        ThrowIfGreaterThan(width, Width);
         if (!_isUpdated) Build();
         return _sum[height * (Width + 1) + width];
     }
 
-
-    public long Sum(int height1, int width1, int height2, int width2)
+    /// <summary>
+    /// Calculate a two-dimensional cumulative sum of [height1, height2), [width1, width2).
+    /// </summary>
+    public T Sum(int height1, int width1, int height2, int width2)
     {
-        if (height1 < 0 || Height < height1) throw new ArgumentOutOfRangeException(nameof(height1));
-        if (width1 < 0 || Width < width1) throw new ArgumentOutOfRangeException(nameof(width1));
-        if (height2 < 0 || Height < height2) throw new ArgumentOutOfRangeException(nameof(height2));
-        if (width2 < 0 || Width < width2) throw new ArgumentOutOfRangeException(nameof(width2));
+        ThrowIfGreaterThanOrEqual(height1, height2);
+        ThrowIfGreaterThanOrEqual(width1, width2);
         if (!_isUpdated) Build();
         var w1 = Width + 1;
         return _sum[height1 * w1 + width1]
@@ -77,5 +90,20 @@ public class CumulativeSum2D
                 + _sum[(i - 1) * w1 + j]
                 - _sum[(i - 1) * w1 + (j - 1)]
                 + _data[(i - 1) * Width + (j - 1)];
+    }
+
+    private static void ThrowIfNegative(int value)
+    {
+        if (value < 0) throw new ArgumentOutOfRangeException(nameof(value));
+    }
+
+    private static void ThrowIfGreaterThan(int value, int other)
+    {
+        if (value > other) throw new ArgumentOutOfRangeException(nameof(value));
+    }
+
+    private static void ThrowIfGreaterThanOrEqual(int value, int other)
+    {
+        if (value >= other) throw new ArgumentOutOfRangeException(nameof(value));
     }
 }
